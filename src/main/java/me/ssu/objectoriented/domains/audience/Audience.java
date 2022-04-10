0c2db0d6c2e95c2e75e317f177b7e453e3d4407a
zzanggoon8 <zzanggoon8@gmail.com>
2022-04-11 02:12:28 +0900

    fourth Step, 소지품을 보관할 수 있는 관람객 구현

--- src/main/java/me/ssu/objectoriented/domains/audience/Audience.java
+++ src/main/java/me/ssu/objectoriented/domains/audience/Audience.java
@@ -3,23 +3,22 @@ package me.ssu.objectoriented.domains.audience;
 import me.ssu.objectoriented.domains.bag.Bag;
 
 /**
- * Fourth Step, 관람객
- * 소지품을 보관할 수 있는 관람객
+ * Fourth Step, 소지품을 보관할 수 있는 관람객
  *
  */
 public class Audience {
 
     private Bag bag;    // 가방
 
-    // TODO 가방을 소유할 수 있는 관람객
+    /**
+     * 가방을 소유할 수 있는 관람객 객체 상태
+     * @param bag
+     */
     public Audience(Bag bag) {
         this.bag = bag;
     }
 
-    /**
-     * 가방을 소지할 수 있는 관람객
-     * @return
-     */
+    // TODO 가방을 소지할 수 있는 관람객
     public Bag getBag() {
         return bag;
     }
