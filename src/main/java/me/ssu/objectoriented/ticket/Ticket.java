4ee7d0dd1eba410210d2e8a7e3985e39c25266ac
zzanggoon8 <zzanggoon8@gmail.com>
2022-04-11 01:22:52 +0900

    second step, 공연을 관람하기 원하는 모든 사람들을 위한 티켓 구현

---
+++ src/main/java/me/ssu/objectoriented/ticket/Ticket.java
@@ -0,0 +1,13 @@
+package me.ssu.objectoriented.ticket;
+
+/**
+ * Second Step
+ *  공연을 관라하기 원하는 모든 사람들은 티켓을 소지하고 있어야 함.
+ */
+public class Ticket {
+    private Long fee;   // 요금
+
+    public Long getFee() {
+        return fee;
+    }
+}
