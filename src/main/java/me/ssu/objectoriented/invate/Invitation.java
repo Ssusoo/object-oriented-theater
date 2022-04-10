0042feadad75d461412d4a2f859ad7464632d5a5
zzanggoon8 <zzanggoon8@gmail.com>
2022-04-11 01:22:01 +0900

    first step, 이벤트 당첨자에게 발송되는 초대장 구현

---
+++ src/main/java/me/ssu/objectoriented/invate/Invitation.java
@@ -0,0 +1,12 @@
+package me.ssu.objectoriented.invate;
+
+import java.time.LocalDateTime;
+
+/**
+ * First Step
+ *  이벤트 당첨자에게 발송되는 초대장을 구현
+ *  공연을 관람할 수 있는 초대일자(when)
+ */
+public class Invitation {
+    private LocalDateTime when; // 초대일자
+}
