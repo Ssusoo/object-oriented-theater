4fa171d25b6b63f49a1c259bf9f12bfe6c5b45b6
zzanggoon8 <zzanggoon8@gmail.com>
2022-04-11 02:08:15 +0900

    fifth, 소극장에 입장하기 위한 매표소 구현

---
+++ src/main/java/me/ssu/objectoriented/domains/ticket/TicketOffice.java
@@ -0,0 +1,37 @@
+package me.ssu.objectoriented.domains.ticket;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Fifth, 소극장에 입장하기 위한 매표소
+ *  1) 초대장을 티켓으로 교환
+ *  2) 현금으로 티켓 구매
+ *  *매표소에는 관람객에게 판매할 티켓과 티켓의 판매 금액이 보관
+ */
+public class TicketOffice {
+
+    private Long amount;                                // 판매금액
+    private List<Ticket> tickets = new ArrayList<>();   // 판매하거나 교환해 줄 티켓 목록
+
+    /**
+     *
+     * @param amount
+     * @param tickets
+     */
+    public TicketOffice(Long amount, Ticket ... tickets) {
+        this.amount = amount;
+        this.tickets.addAll(Arrays.asList(tickets));
+    }
+
+    // 판매 금액 더하기
+    public void plusAccount(Long amount) {
+        this.amount += amount;
+    }
+
+    // 판매 금액 빼기
+    public void minusAccount(Long amount) {
+        this.amount -= amount;
+    }
+}
