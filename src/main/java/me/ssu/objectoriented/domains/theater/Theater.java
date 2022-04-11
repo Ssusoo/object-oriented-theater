a61c4f41ddda5d199347e7f43f53f4bfd3c65e2a
zzanggoon8 <zzanggoon8@gmail.com>
2022-04-11 14:24:46 +0900

    8th, 극장의 결합도 낮추기 -> 판매원이랑 역할 나누기

--- src/main/java/me/ssu/objectoriented/domains/theater/Theater.java
+++ src/main/java/me/ssu/objectoriented/domains/theater/Theater.java
@@ -1,12 +1,15 @@
 package me.ssu.objectoriented.domains.theater;
 
 import me.ssu.objectoriented.domains.audience.Audience;
-import me.ssu.objectoriented.domains.ticket.Ticket;
 import me.ssu.objectoriented.domains.ticket.TicketSeller;
 
 /**
  * Seventh, 소극장을 구현하는 Theater
  *  소극장은 먼저 관람객의 가방 안에 초대장이 들어 있는지 확인
+ *
+ * 8th, 극장의 결합도 낮추기 -> 판매원이랑 역할 나누기
+ *  이벤트 관람객, 일반 관람객,
+ *  판매원 초대장을 티켓으로 티켓을 판매하는 로직 처리의 결합도를 낮추기
  */
 public class Theater {
 
@@ -25,28 +28,34 @@ public class Theater {
      * @param audience
      */
     public void enter(Audience audience) {
-        // TODO 초대장이 있는 이벤트 관람객이면
-        if (audience.getBag().hasInvitation()) {
+        ticketSeller.sellTO(audience);
 
-            // TODO 판매원은 매표소에서 초대장을 티켓으로 교환해줌.
-            Ticket ticket = ticketSeller.getTicketOffice().getTickets();
-
-            // TODO 이벤트 관람객이면 초대장을 티켓으로 교환 후 가방에 넣음.
-            audience.getBag().setTicket(ticket);
-
-        // TODO 초대장이 없는 일반 관람객이면
-        } else {
-            // TODO 판매원은 매표소에서 티켓을 판매함.
-            Ticket ticket = ticketSeller.getTicketOffice().getTickets();
-
-            // TODO 일반 관람객은 가방에서 티켓 금액만큼 차감.
-            audience.getBag().minusAmount(ticket.getFee());
-
-            // TODO 판매원은 매표소에 티켓 금액만큼 증가.
-            ticketSeller.getTicketOffice().plusAccount(ticket.getFee());
-
-            // TODO 일반 관람객은 돈을 티켓으로 교환 후 가방에 넣음.
-            audience.getBag().setTicket(ticket);
-        }
+        // TODO 결함도를 낮추기 위해 TicketSeller 클래스로 옮기기
+        //  초대장이 있는 이벤트 관람객과 일반 관람객의 상태와
+        //  판매원이 초대장을 티켓으로 교환하는 상황과 티켓을 구매하는 방식의
+        //  의존성을 극장에게 위임하는 게 아니라 판매원에게로 넘겨 결합도 낮추기
+        // 초대장이 있는 이벤트 관람객이면
+//        if (audience.getBag().hasInvitation()) {
+//            // 판매원은 매표소에서 초대장을 티켓으로 교환해줌.
+//            Ticket ticket = ticketSeller.getTicketOffice().getTickets();
+//
+//            // 이벤트 관람객이면 초대장을 티켓으로 교환 후 가방에 넣음.
+//            audience.getBag().setTicket(ticket);
+//
+//            // 초대장이 없는 일반 관람객이면
+//            } else {
+//            // 판매원은 매표소에서 티켓을 판매함.
+//            Ticket ticket = ticketSeller.getTicketOffice().getTickets();
+//
+//            // 일반 관람객은 가방에서 티켓 금액만큼 차감.
+//            audience.getBag().minusAmount(ticket.getFee());
+//
+//            // 판매원은 매표소에 티켓 금액만큼 증가.
+//            ticketSeller.getTicketOffice().plusAccount(ticket.getFee());
+//
+//            // 일반 관람객은 돈을 티켓으로 교환 후 가방에 넣음.
+//            audience.getBag().setTicket(ticket);
+//        }
     }
+
 }
--- src/main/java/me/ssu/objectoriented/domains/ticket/TicketSeller.java
+++ src/main/java/me/ssu/objectoriented/domains/ticket/TicketSeller.java
@@ -1,5 +1,7 @@
 package me.ssu.objectoriented.domains.ticket;
 
+import me.ssu.objectoriented.domains.audience.Audience;
+
 /**
  * Sixth Step, 판매원
  * 자신이 일하는 매표소를 알고 있어야 함.
@@ -18,9 +20,40 @@ public class TicketSeller {
 
     /**
      * 매표소의 상태를 볼 수 있는 판매원
+     * TicketSeller에서 getTicketOffice 메서드가 제거된 이유?
+     *  ticketOffice에 대한 접근은 오직 TicketSeller 안에만 존재하게 됨.
      * @return
      */
-    public TicketOffice getTicketOffice() {
-        return ticketOffice;
+//    public TicketOffice getTicketOffice() {
+//        return ticketOffice;
+//    }
+
+    public void sellTO(Audience audience) {
+        // TODO 초대장이 있는 이벤트 관람객이면
+        if (audience.getBag().hasInvitation()) {
+
+            // TODO 판매원은 매표소에서 초대장을 티켓으로 교환해줌.
+//            Ticket ticket = ticketSeller.getTicketOffice().getTickets();
+            Ticket ticket = ticketOffice.getTickets();
+
+            // TODO 이벤트 관람객이면 초대장을 티켓으로 교환 후 가방에 넣음.
+            audience.getBag().setTicket(ticket);
+
+        // TODO 초대장이 없는 일반 관람객이면
+        } else {
+            // TODO 판매원은 매표소에서 티켓을 판매함.
+//            Ticket ticket = ticketSeller.getTicketOffice().getTickets();
+            Ticket ticket = ticketOffice.getTickets();
+
+            // TODO 일반 관람객은 가방에서 티켓 금액만큼 차감.
+            audience.getBag().minusAmount(ticket.getFee());
+
+            // TODO 판매원은 매표소에 티켓 금액만큼 증가.
+//            ticketSeller.getTicketOffice().plusAccount(ticket.getFee());
+            ticketOffice.plusAccount(ticket.getFee());
+
+            // TODO 일반 관람객은 돈을 티켓으로 교환 후 가방에 넣음.
+            audience.getBag().setTicket(ticket);
+        }
     }
 }
