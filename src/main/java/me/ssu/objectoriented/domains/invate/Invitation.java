14879d7ff0ab39e0b2dd6ae6d4190d0ddc6f3046
zzanggoon8 <zzanggoon8@gmail.com>
2022-04-11 14:45:57 +0900

    1th Step, 이벤트 당첨자에게 발송되는 초대장을 구현

--- src/main/java/me/ssu/objectoriented/domains/invate/Invitation.java
+++ src/main/java/me/ssu/objectoriented/domains/invate/Invitation.java
@@ -3,8 +3,7 @@ package me.ssu.objectoriented.domains.invate;
 import java.time.LocalDateTime;
 
 /**
- * First Step
- *  이벤트 당첨자에게 발송되는 초대장을 구현
+ * 1th Step, 이벤트 당첨자에게 발송되는 초대장을 구현
  *  공연을 관람할 수 있는 초대일자(when)
  */
 public class Invitation {
