087f674d21b2e4ac32ac891dfef73d6355ba30f3
zzanggoon8 <zzanggoon8@gmail.com>
2022-04-11 14:48:18 +0900

    4th, 소극장에 입장하기 위한 매표소

--- src/main/java/me/ssu/objectoriented/domains/ticket/TicketOffice.java
+++ src/main/java/me/ssu/objectoriented/domains/ticket/TicketOffice.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- * Fifth, 소극장에 입장하기 위한 매표소
+ * 4th, 소극장에 입장하기 위한 매표소
  *  1) 초대장을 티켓으로 교환
  *  2) 현금으로 티켓 구매
  *  *매표소에는 관람객에게 판매할 티켓과 티켓의 판매 금액이 보관
@@ -16,7 +16,7 @@ public class TicketOffice {
     private List<Ticket> tickets = new ArrayList<>();   // 판매하거나 교환해 줄 티켓 목록
 
     /**
-     * 소극장에 입장하기 위한 매표소 객체 상태
+     * 4th, 소극장에 입장하기 위한 매표소 객체 상태
      * @param amount
      * @param tickets
      */
@@ -26,7 +26,7 @@ public class TicketOffice {
     }
 
     /**
-     * 티켓을 판매하는 메서드
+     * 4th, 티켓을 판매하는 메서드
      * @return
      */
     public Ticket getTickets() {
