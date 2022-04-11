5601b4f2b29414bf38d40cf4815af713dd7b3b26
zzanggoon8 <zzanggoon8@gmail.com>
2022-04-11 16:13:21 +0900

    10th, TicketSeller의 결합도를 낮춘 설계

--- src/main/java/me/ssu/objectoriented/domains/audience/Audience.java
+++ src/main/java/me/ssu/objectoriented/domains/audience/Audience.java
@@ -1,10 +1,14 @@
 package me.ssu.objectoriented.domains.audience;
 
 import me.ssu.objectoriented.domains.bag.Bag;
+import me.ssu.objectoriented.domains.ticket.Ticket;
 
 /**
  * 4th, 소지품을 보관할 수 있는 관람객
+ *  외부의 제3자가 자신의 가방을 열어보도록 허용
  *
+ * 11th, Audience의 결합도를 낮춘 설계
+ *  외부의 제3자가 자신의 가방을 열어보도록 허용하지 않음.
  */
 public class Audience {
 
@@ -19,7 +23,27 @@ public class Audience {
     }
 
     // TODO 가방을 소지할 수 있는 관람객
-    public Bag getBag() {
-        return bag;
+//    public Bag getBag() {
+//        return bag;
+//    }
+
+    /**
+     * 11th, Audience의 결합도를 낮춘 설계
+     * @param ticket
+     * @return
+     */
+    public Long buy(Ticket ticket) {
+        // TODO 11th, 초대장이 있는 이벤트 관람객이라면
+        if (bag.hasInvitation()) {
+            // TODO 초대장 있으면 초대장을 티켓으로 교환
+            bag.setTicket(ticket);
+            return 0L;
+        // TODO 11th, 초대장이 없는 관람객이라면
+        } else {
+            // TODO 초대장이 없으면 현금으로 티켓 구매
+            bag.minusAmount(ticket.getFee());
+            return ticket.getFee();
+        }
     }
+
 }
--- src/main/java/me/ssu/objectoriented/domains/ticket/TicketSeller.java
+++ src/main/java/me/ssu/objectoriented/domains/ticket/TicketSeller.java
@@ -6,7 +6,10 @@ import me.ssu.objectoriented.domains.audience.Audience;
  * 6th Step, 판매원
  *  자신이 일하는 매표소를 알고 있어야 함.
  *
- * 9th, 극장에게 받은 로직 결합도를 받아옴.
+ * 9th, Theater의 결합도를 낮춘 설계.
+ * 
+ * 10th, TicketSeller의 결합도를 낮춘 설계
+ *  *Audience에게로 넘겨주면서 결합도를 줄여보기
  */
 public class TicketSeller {
 
@@ -34,40 +37,44 @@ public class TicketSeller {
     /**
      * 9th, 극장의 결함도를 낮춤
      *  ticketOffice에 대한 접근은 오직 TicketSeller안에만 존재.
+     *
+     * 10th, TicketSeller의 결합도를 낮춘 설계
      * @param audience
      */
     public void sellTO(Audience audience) {
-        // TODO 초대장이 있는 이벤트 관람객이면
-        if (audience.getBag().hasInvitation()) {
+        // TODO 10th, TicketSeller의 결합도를 낮춘 설계
+        ticketOffice.plusAccount(audience.buy(ticketOffice.getTickets()));
 
-            // TODO 판매원은 매표소에서 초대장을 티켓으로 교환해줌.
-//            Ticket ticket = ticketSeller.getTicketOffice().getTickets();
-            // TODO 결합도를 낮춘 후
-            //  ticketOffice에 대한 접근은 오직 TicketSeller 안에만 존재하게 됨.
-            Ticket ticket = ticketOffice.getTickets();
-
-            // TODO 이벤트 관람객이면 초대장을 티켓으로 교환 후 가방에 넣음.
-            audience.getBag().setTicket(ticket);
-
-        // TODO 초대장이 없는 일반 관람객이면
-        } else {
-            // TODO 판매원은 매표소에서 티켓을 판매함.
-//            Ticket ticket = ticketSeller.getTicketOffice().getTickets();
-            // TODO 결합도를 낮춘 후
-            //  ticketOffice에 대한 접근은 오직 TicketSeller 안에만 존재하게 됨.
-            Ticket ticket = ticketOffice.getTickets();
-
-            // TODO 일반 관람객은 가방에서 티켓 금액만큼 차감.
-            audience.getBag().minusAmount(ticket.getFee());
-
-            // TODO 판매원은 매표소에 티켓 금액만큼 증가.
-//            ticketSeller.getTicketOffice().plusAccount(ticket.getFee());
-            // TODO 결합도를 낮춘 후
-            //  ticketOffice에 대한 접근은 오직 TicketSeller 안에만 존재하게 됨.
-            ticketOffice.plusAccount(ticket.getFee());
-
-            // TODO 일반 관람객은 돈을 티켓으로 교환 후 가방에 넣음
-            audience.getBag().setTicket(ticket);
-        }
+//        // 9th, 초대장이 있는 이벤트 관람객이면
+//        if (audience.getBag().hasInvitation()) {
+//            // 판매원은 매표소에서 초대장을 티켓으로 교환해줌.
+////            Ticket ticket = ticketSeller.getTicketOffice().getTickets();
+//            // 결합도를 낮춘 후
+//            //  ticketOffice에 대한 접근은 오직 TicketSeller 안에만 존재하게 됨.
+//            Ticket ticket = ticketOffice.getTickets();
+//
+//            // 이벤트 관람객이면 초대장을 티켓으로 교환 후 가방에 넣음.
+//            audience.getBag().setTicket(ticket);
+//
+//        // 초대장이 없는 일반 관람객이면
+//        } else {
+//            // 판매원은 매표소에서 티켓을 판매함.
+////            Ticket ticket = ticketSeller.getTicketOffice().getTickets();
+//            // 결합도를 낮춘 후
+//            //  ticketOffice에 대한 접근은 오직 TicketSeller 안에만 존재하게 됨.
+//            Ticket ticket = ticketOffice.getTickets();
+//
+//            // 일반 관람객은 가방에서 티켓 금액만큼 차감.
+//            audience.getBag().minusAmount(ticket.getFee());
+//
+//            // 판매원은 매표소에 티켓 금액만큼 증가.
+////            ticketSeller.getTicketOffice().plusAccount(ticket.getFee());
+//            // 결합도를 낮춘 후
+//            //  ticketOffice에 대한 접근은 오직 TicketSeller 안에만 존재하게 됨.
+//            ticketOffice.plusAccount(ticket.getFee());
+//
+//            // 일반 관람객은 돈을 티켓으로 교환 후 가방에 넣음
+//            audience.getBag().setTicket(ticket);
+//        }
     }
 }
