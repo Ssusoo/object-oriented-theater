1e3c71407d0cc19a63c013d684a35ef3346da50c
김수환 <zzanggoon8@hiconsy.com>
2022-04-15 21:18:36 +0900

    9th, 금액 할인 정책

--- src/main/java/me/ssu/objectoriented/movie/domains/policy/AmountDiscountPolicy.java
+++ src/main/java/me/ssu/objectoriented/movie/domains/policy/AmountDiscountPolicy.java
@@ -5,7 +5,7 @@ import me.ssu.objectoriented.movie.domains.money.Money;
 import me.ssu.objectoriented.movie.domains.screening.Screening;
 
 /**
- * 9th, 할인 정책 구현
+ * 9th, 금액 할인 정책
  *  할인 조건을 만족할 경우 일정한 금액을 할인해주는
  *  금액 할인 정책을 구현.
  *
