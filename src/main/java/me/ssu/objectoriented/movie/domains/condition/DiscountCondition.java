0c2f745dee7b0c7185fb316a0cc6c96f695faa27
김수환 <zzanggoon8@hiconsy.com>
2022-04-15 20:41:23 +0900

    6th, 할인 조건 인터페이스

--- src/main/java/me/ssu/objectoriented/movie/domains/condition/DiscountCondition.java
+++ src/main/java/me/ssu/objectoriented/movie/domains/condition/DiscountCondition.java
@@ -3,7 +3,8 @@ package me.ssu.objectoriented.movie.domains.condition;
 import me.ssu.objectoriented.movie.domains.screening.Screening;
 
 /**
- * 6th,
+ * 6th, 할인 조건 인터페이스
+ *  Screening 클래스에서 상속 받아 구현해서 사용할 거임.
  */
 public interface DiscountCondition {
     boolean isSatisfiedBy(Screening screening);
