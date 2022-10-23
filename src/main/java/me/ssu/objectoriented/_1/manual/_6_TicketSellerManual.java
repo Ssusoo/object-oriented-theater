306199e9e1fefe71b393736951b85a6c4367de6e
zzanggoon8 <zzanggoon8@gmail.com>
2022-10-23 21:16:57 +0900

    1th, 관람객을 유치할 수 있는 로직(Detail Manual 세부설명 : 오타 수정)

--- src/main/java/me/ssu/objectoriented/_1/manual/_6_TicketSellerManual.java
+++ src/main/java/me/ssu/objectoriented/_1/manual/_6_TicketSellerManual.java
@@ -4,13 +4,13 @@ public class _6_TicketSellerManual {
 /* 6. 판매원(TicketSeller)
 	: 판매원은 매표소에서 초대장을 티켓으로 교환해 주거나
 	: 티켓을 판매하는 역할을 수행한다.
-	: 판매원 클래스는 자신이 일하는 매표소(TicketOfiice)를 알고 있어야 한다.
+	: 판매원 클래스는 자신이 일하는 매표소(TicketOffice)를 알고 있어야 한다.
 	6-1) 변수
 		6-1)-1. 매표소(Ticket Office)
 	6-2) 생성자
 		6-2)-1. 판매원, TicketSeller(매표소)
 	6-3) 메소드
-		6-3)-1. 매표소 getTIcketOffice
-		-> 판매원 클래스는 자신이 일하는 매표소(TicketOfiice)를 알고 있어야 한다.
+		6-3)-1. 매표소 getTicketOffice
+		-> 판매원 클래스는 자신이 일하는 매표소(TicketOffice)를 알고 있어야 한다.
 */
 }
