6fbc2566227343ee3825980ecd4b1ece5b703912
zzanggoon8 <zzanggoon8@gmail.com>
2022-10-24 23:04:03 +0900

    1th, 관람객을 유치할 수 있는 로직의 문제점(다른 방향에서 생겨버린 의존성과 결합도)

---
+++ src/main/java/me/ssu/objectoriented/_1/problem/ProblemOfDependencyBetweenTicketOfficeAndAudience.java
@@ -0,0 +1,19 @@
+package me.ssu.objectoriented._1.problem;
+
+public class ProblemOfDependencyBetweenTicketOfficeAndAudience {
+/*
+	TODO TicketOffice와 Audience의 의존성 추가 이유?
+		변경 전에는 TicketOffice와 Audience에 대해 알지 못했다.
+		변경 후에는 TicketOffice와 Audience에게 직접 티켓을 판매하기에 Audience에 관해 알고 있다.
+
+		변경 전에는 존재하지 않은 새로운 의존성이 추가된 거다.
+		의존성의 추가는 높은 결합도를 의미하고,
+		높은 결합도는 변경하기 어려운 설계를 의미한다.
+		TicketOffice의 자율성은 높였지만 TODO "전체 설계 관점에서 결합도사 상승했다."
+
+	TODO 이를 통해 알게 된 사실?
+		1) 어떤 기능을 설계하는 방법은 한 가지 이상일 수 있다.
+		2) 동일한 기능을 한 가지 이상의 방법으로 설계할 수 있기에 결국 설계는 트레이드오프의 산물이다.
+		"어떤 경우에도 모든 사람들을 만족시킬 수 있는 설계를 만들 수 없다."
+*/
+}
