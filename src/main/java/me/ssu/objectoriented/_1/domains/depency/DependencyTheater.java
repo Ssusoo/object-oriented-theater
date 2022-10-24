df0cc4647b21269df694b6126a47abb3045e3bb3
zzanggoon8 <zzanggoon8@gmail.com>
2022-10-24 19:11:17 +0900

    1th, 관람객을 유치할 수 있는 로직(7) 소극장(Theater) -> DependencyTheater로 클래스 이름 수정

--- src/main/java/me/ssu/objectoriented/_1/domains/depency/DependencyTheater.java
+++ src/main/java/me/ssu/objectoriented/_1/domains/depency/DependencyTheater.java
@@ -1,15 +1,20 @@
-package me.ssu.objectoriented._1.domains;
+package me.ssu.objectoriented._1.domains.depency;
+
+import me.ssu.objectoriented._1.domains.Audience;
+import me.ssu.objectoriented._1.domains.Ticket;
+import me.ssu.objectoriented._1.domains.TicketSeller;
 
 // 소극장(Theater)
-public class Theater {
+public class DependencyTheater {
 	private TicketSeller ticketSeller;
 
 	// TODO 1) 소극장에는 판매원이 있어야 함.
-	public Theater(TicketSeller ticketSeller) {
+	public DependencyTheater(TicketSeller ticketSeller) {
 		this.ticketSeller = ticketSeller;
 	}
 
 	// TODO 2) 입구(enter)
+	//  입구 로직 때문에 의존성이 높다. Theater -> DependencyTheater로 클래스 수정
 	public void enter(Audience audience) {
 		// 관람객 가방에 초대권이 있으면 이벤트에 당첨된 관람객
 		if (audience.getBag().hasInvitation()) {
