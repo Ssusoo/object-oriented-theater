dafe342103549bd2a19a2044ba1a7af2ec286d68
zzanggoon8 <zzanggoon8@gmail.com>
2022-10-24 20:40:59 +0900

    1th, 관람객을 유치할 수 있는 로직(7) 소극장(Theater) -> AutonomyTheater의 입구(enter) 메소드 파라미터 수정

--- src/main/java/me/ssu/objectoriented/_1/domains/autonomy/AutonomyTheater.java
+++ src/main/java/me/ssu/objectoriented/_1/domains/autonomy/AutonomyTheater.java
@@ -1,6 +1,7 @@
 package me.ssu.objectoriented._1.domains.autonomy;
 
 import me.ssu.objectoriented._1.domains.Audience;
+import me.ssu.objectoriented._1.domains.encapsulation.EncapsulationAudience;
 
 // 자율성이 높은 소극장(AutonomyTheater)
 public class AutonomyTheater {
@@ -12,13 +13,17 @@ public class AutonomyTheater {
 		this.autonomyTicketSeller = autonomyTicketSeller;
 	}
 
-	// TODO 2) 의존성이 높은 소극장의 입구 메소드 -> 판매원에게 역할로 주기
+	// TODO 2-1) 의존성이 높은 소극장의 입구 메소드 -> 판매원에게 역할로 주기
 	//  입구(enter) 메소드 의존성 낮추기
 	//  소극장(AutonomyTheater)은 더 이상 매표소(TicketOffice)에 접근하지 않는다.
 	// TODO 결론?
 	//  1) 소극장의 입장에서 매표소가 판매원 내부에 존재한다는 사실을 모른다.
 	//  2) 단지, 소극장의 상황에서는 판매원이 sellTo() 메소드를 이해하고 응답할 수 있다는 사실만 알고 있다.
-	public void enter(Audience audience) {
-		autonomyTicketSeller.sellTo(audience);
+	//	public void enter(Audience audience) {
+	//		autonomyTicketSeller.sellTo(audience);
+	//	}
+	// TODO 2-2) 캡슐화한 관람객으로 인해 메소드 파라미터 수정
+	public void enter(EncapsulationAudience encapsulationAudience) {
+		autonomyTicketSeller.sellToEncapsulationAudience(encapsulationAudience);
 	}
 }
